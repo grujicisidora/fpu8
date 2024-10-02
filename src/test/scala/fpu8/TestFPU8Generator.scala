a4fe892300f3bc85acf9e8a7b05fc20670889cbc
Isidora Grujic <grujic.isidora2@gmail.com>
2024-10-02 19:27:21 +0200

    Made changes in FPU8Top in accordance with FPU8Generator implementation.

--- src/test/scala/fpu8/TestFPU8Generator.scala
+++ src/test/scala/fpu8/TestFPU8Generator.scala
@@ -161,4 +161,160 @@ class TestFPU8Generator extends AnyFlatSpec with ChiselScalatestTester {
       dut.z.expect(89.U)
     }
   }
+
+  it should "also calculate various expressions between numbers in a E5M2 fp8 representation." in {
+    test(new FPU8Generator(true)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.enable.poke(0.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(4.U) // <
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(0.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(4.U) // <
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(5.U) // >
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(0.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(6.U) // ==
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(0.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(7.U) // <=
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(42.U)
+      dut.opCode.poke(8.U) // >=
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(9.U) // !=
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(75.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(170.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(75.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(79.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(79.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(75.U)
+      dut.b.data.poke(42.U)
+      dut.opCode.poke(1.U) // -
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(75.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(42.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(2.U) // *
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(58.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(170.U)
+      dut.b.data.poke(75.U)
+      dut.opCode.poke(2.U) // *
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(186.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.a.data.poke(75.U)
+      dut.b.data.poke(42.U)
+      dut.opCode.poke(3.U) // /
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(93.U)
+    }
+  }
 }
--- src/test/scala/fpu8/TestFPU8Top.scala
+++ src/test/scala/fpu8/TestFPU8Top.scala
@@ -8,6 +8,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 class TestFPU8Top extends AnyFlatSpec with ChiselScalatestTester {
   "FPU8Top" should "calculate various expressions between numbers in two different fp8 representations." in {
     test(new FPU8Top).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // e4m3 encoding
       dut.enable.poke(0.U)
       dut.encoding.poke(0.U)
       dut.a.poke(42.U)
@@ -89,6 +90,258 @@ class TestFPU8Top extends AnyFlatSpec with ChiselScalatestTester {
       dut.saturationMode.poke(0.U)
       dut.clock.step()
       dut.z.expect(56.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(76.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(170.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(74.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(42.U)
+      dut.b.poke(79.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(80.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(75.U)
+      dut.b.poke(42.U)
+      dut.opCode.poke(1.U) // -
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(74.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(2.U) // *
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(62.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(170.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(2.U) // *
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(190.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(0.U)
+      dut.a.poke(75.U)
+      dut.b.poke(42.U)
+      dut.opCode.poke(3.U) // /
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(89.U)
+
+      // e5m2 encoding
+
+      dut.enable.poke(0.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(4.U) // <
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(0.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(4.U) // <
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(5.U) // >
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(0.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(6.U) // ==
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(0.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(7.U) // <=
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(42.U)
+      dut.opCode.poke(8.U) // >=
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(9.U) // !=
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(60.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(75.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(170.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(75.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(79.U)
+      dut.opCode.poke(0.U) // +
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(79.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(75.U)
+      dut.b.poke(42.U)
+      dut.opCode.poke(1.U) // -
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(75.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(42.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(2.U) // *
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(58.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(170.U)
+      dut.b.poke(75.U)
+      dut.opCode.poke(2.U) // *
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(186.U)
+
+      dut.clock.step()
+
+      dut.enable.poke(1.U)
+      dut.encoding.poke(1.U)
+      dut.a.poke(75.U)
+      dut.b.poke(42.U)
+      dut.opCode.poke(3.U) // /
+      dut.roundingMode.poke(0.U)
+      dut.saturationMode.poke(0.U)
+      dut.clock.step()
+      dut.z.expect(93.U)
     }
   }
 }
